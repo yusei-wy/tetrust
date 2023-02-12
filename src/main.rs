26dfe9091af9abccd6b5f18e2617aa541aa663c0
yusei-wy <yusei.kasa@gmail.com>
2023-02-12 22:26:17 +0900

    render block

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    let block = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]];
+
     let field = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -23,9 +25,20 @@ fn main() {
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
 
+    // generate field for render
+    let mut field_buf = field;
+    for y in 0..4 {
+        for x in 0..4 {
+            if block[y][x] == 1 {
+                field_buf[y + 8][x + 4] = 1;
+            }
+        }
+    }
+
+    // render field
     for y in 0..21 {
         for x in 0..13 {
-            if field[y][x] == 1 {
+            if field_buf[y][x] == 1 {
                 print!("[]");
             } else {
                 print!(" .");
