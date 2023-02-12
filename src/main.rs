266fd71c5de78b31319e5a87f4ba16b8a5a77017
yusei-wy <yusei.kasa@gmail.com>
2023-02-12 22:39:20 +0900

    render all blocks

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,68 @@
-fn main() {
-    let block = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]];
+enum BlockKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
 
+type BlockShape = [[usize; 4]; 4];
+#[rustfmt::skip]
+const BLOCKS: [BlockShape; 7] = [
+    // I block
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+    ],
+    // O block
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    // S block
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [1, 1, 0, 0],
+        [0, 0, 0, 0],
+    ],
+    // Z block
+    [
+        [0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    // J block
+    [
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    // L block
+    [
+        [0, 0, 0, 0],
+        [0, 0, 1, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    // T block
+    [
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+];
+
+fn main() {
     let field = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -29,9 +91,13 @@ fn main() {
     let mut field_buf = field;
     for y in 0..4 {
         for x in 0..4 {
-            if block[y][x] == 1 {
-                field_buf[y + 8][x + 4] = 1;
-            }
+            field_buf[y + 2][x + 2] = BLOCKS[BlockKind::I as usize][y][x];
+            field_buf[y + 2][x + 7] = BLOCKS[BlockKind::O as usize][y][x];
+            field_buf[y + 6][x + 2] = BLOCKS[BlockKind::S as usize][y][x];
+            field_buf[y + 6][x + 7] = BLOCKS[BlockKind::Z as usize][y][x];
+            field_buf[y + 10][x + 2] = BLOCKS[BlockKind::J as usize][y][x];
+            field_buf[y + 10][x + 7] = BLOCKS[BlockKind::L as usize][y][x];
+            field_buf[y + 14][x + 2] = BLOCKS[BlockKind::T as usize][y][x];
         }
     }
 
