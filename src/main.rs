a417f4369745cf8a8da898ddd2368178307aed16
yusei-wy <yusei.kasa@gmail.com>
2023-04-24 21:10:40 +0900

    change: improved response

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
 mod entity;
+
 use getch_rs::{Getch, Key};
+use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
 use entity::*;
+
+fn draw(field: &Field, pos: &Position) {
+    // create draw field
+    let mut field_buf = field.clone();
+    for y in 0..4 {
+        for x in 0..4 {
+            if BLOCKS[BlockKind::I as usize][y][x] == 1 {
+                field_buf[y + pos.y][x + pos.x] = 1;
+            }
+        }
+    }
+
+    clear_cursor_pos();
+
+    for y in 0..FIELD_HEIGHT {
+        for x in 0..FIELD_WIDTH {
+            if field_buf[y][x] == 1 {
+                print!("[]");
+            } else {
+                print!(" .");
+            }
+        }
+        println!();
+    }
+}
 
 // TODO: enum でリストしてコマンドにしたい
 // CSI シーケンスによって端末を操作している
@@ -54,83 +81,76 @@ fn main() {
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
 
-    let mut pos = Position { x: 4, y: 0 };
-    let g = Getch::new();
+    let pos = Arc::new(Mutex::new(Position { x: 4, y: 0 }));
 
     clear_screen();
+    draw(&field, &pos.lock().unwrap());
 
-    loop {
-        // generate field for render
-        let mut field_buf = field;
+    // natural fall process
+    {
+        let pos = Arc::clone(&pos);
+        let _ = thread::spawn(move || {
+            loop {
+                thread::sleep(time::Duration::from_millis(1000));
 
-        // natural fall
-        let new_pos = Position {
-            x: pos.x,
-            y: pos.y + 1,
-        };
+                // natural fall
+                let mut pos = pos.lock().unwrap();
+                let new_pos = Position {
+                    x: pos.x,
+                    y: pos.y + 1,
+                };
 
-        if !is_collision(&field, &new_pos, BlockKind::I) {
-            // updated pos
-            pos = new_pos;
-        }
-
-        // set block info in the drawing field
-        for y in 0..4 {
-            for x in 0..4 {
-                if BLOCKS[BlockKind::I as usize][y][x] == 1 {
-                    field_buf[y + pos.y][x + pos.x] = 1;
+                if !is_collision(&field, &new_pos, BlockKind::I) {
+                    // updated pos
+                    *pos = new_pos;
                 }
+                draw(&field, &pos);
             }
-        }
-
-        // render field
-        clear_cursor_pos();
-        (0..FIELD_HEIGHT).for_each(|y| {
-            (0..FIELD_WIDTH).for_each(|x| {
-                if field_buf[y][x] == 1 {
-                    print!("[]");
-                } else {
-                    print!(" .");
-                }
-            });
-            println!();
         });
+    }
 
-        thread::sleep(time::Duration::from_millis(1000));
-
+    let g = Getch::new();
+    loop {
         // waiting for key input
         match g.getch() {
             Ok(Key::Left) => {
+                let mut pos = pos.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x - 1,
                     y: pos.y,
                 };
                 if !is_collision(&field, &new_pos, BlockKind::I) {
-                    pos = new_pos;
+                    *pos = new_pos;
                 }
+                draw(&field, &pos);
             }
             Ok(Key::Down) => {
+                let mut pos = pos.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x,
                     y: pos.y + 1,
                 };
                 if !is_collision(&field, &new_pos, BlockKind::I) {
-                    pos = new_pos;
+                    *pos = new_pos;
                 }
+                draw(&field, &pos);
             }
             Ok(Key::Right) => {
+                let mut pos = pos.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x + 1,
                     y: pos.y,
                 };
                 if !is_collision(&field, &new_pos, BlockKind::I) {
-                    pos = new_pos;
+                    *pos = new_pos;
                 }
+                draw(&field, &pos);
             }
-            Ok(Key::Char('q')) => break,
+            Ok(Key::Char('q')) => {
+                show_cursor();
+                return;
+            }
             _ => (),
         }
     }
-
-    show_cursor();
 }
