ac47b358f2a31d82239b02d7dbde4aa242109dd9
yusei-wy <yusei.kasa@gmail.com>
2023-04-29 18:07:11 +0900

    change: display random blocks

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 mod entity;
 
 use getch_rs::{Getch, Key};
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
+impl Distribution<BlockKind> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> BlockKind {
+        match rng.gen_range(0..=6) {
+            0 => BlockKind::I,
+            1 => BlockKind::O,
+            2 => BlockKind::S,
+            3 => BlockKind::Z,
+            4 => BlockKind::J,
+            5 => BlockKind::L,
+            _ => BlockKind::T,
+        }
+    }
+}
+
 use entity::*;
 
-fn draw(field: &Field, pos: &Position) {
+fn draw(field: &Field, pos: &Position, block: BlockKind) {
     // create draw field
     let mut field_buf = field.clone();
     for y in 0..4 {
         for x in 0..4 {
-            if BLOCKS[BlockKind::I as usize][y][x] == 1 {
+            if BLOCKS[block as usize][y][x] == 1 {
                 field_buf[y + pos.y][x + pos.x] = 1;
             }
         }
@@ -81,14 +99,20 @@ fn main() {
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ]));
     let pos = Arc::new(Mutex::new(Position { x: 4, y: 0 }));
+    let block = Arc::new(Mutex::new(rand::random::<BlockKind>()));
 
     clear_screen();
-    draw(&field.lock().unwrap(), &pos.lock().unwrap());
+    draw(
+        &field.lock().unwrap(),
+        &pos.lock().unwrap(),
+        *block.lock().unwrap(),
+    );
 
     // natural fall process
     {
         let pos = Arc::clone(&pos);
         let field = Arc::clone(&field);
+        let block = Arc::clone(&block);
         let _ = thread::spawn(move || {
             loop {
                 thread::sleep(time::Duration::from_millis(1000));
@@ -96,28 +120,30 @@ fn main() {
                 // natural fall
                 let mut pos = pos.lock().unwrap();
                 let mut field = field.lock().unwrap();
+                let mut block = block.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x,
                     y: pos.y + 1,
                 };
 
                 // if you don't update the coordinates first, the block will be fixed
-                if !is_collision(&field, &new_pos, BlockKind::I) {
+                if !is_collision(&field, &new_pos, *block) {
                     // updated pos
                     *pos = new_pos;
                 } else {
                     // fix block to field
                     for y in 0..4 {
                         for x in 0..4 {
-                            if BLOCKS[BlockKind::I as usize][y][x] == 1 {
+                            if BLOCKS[*block as usize][y][x] == 1 {
                                 field[y + pos.y][x + pos.x] = 1;
                             }
                         }
                     }
                     // initialize the coordinates of pos
                     *pos = Position { x: 4, y: 0 };
+                    *block = rand::random();
                 }
-                draw(&field, &pos);
+                draw(&field, &pos, *block);
             }
         });
     }
@@ -129,38 +155,41 @@ fn main() {
             Ok(Key::Left) => {
                 let mut pos = pos.lock().unwrap();
                 let field = field.lock().unwrap();
+                let block = block.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x - 1,
                     y: pos.y,
                 };
-                if !is_collision(&field, &new_pos, BlockKind::I) {
+                if !is_collision(&field, &new_pos, *block) {
                     *pos = new_pos;
                 }
-                draw(&field, &pos);
+                draw(&field, &pos, *block);
             }
             Ok(Key::Down) => {
                 let mut pos = pos.lock().unwrap();
                 let field = field.lock().unwrap();
+                let block = block.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x,
                     y: pos.y + 1,
                 };
-                if !is_collision(&field, &new_pos, BlockKind::I) {
+                if !is_collision(&field, &new_pos, *block) {
                     *pos = new_pos;
                 }
-                draw(&field, &pos);
+                draw(&field, &pos, *block);
             }
             Ok(Key::Right) => {
                 let mut pos = pos.lock().unwrap();
                 let field = field.lock().unwrap();
+                let block = block.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x + 1,
                     y: pos.y,
                 };
-                if !is_collision(&field, &new_pos, BlockKind::I) {
+                if !is_collision(&field, &new_pos, *block) {
                     *pos = new_pos;
                 }
-                draw(&field, &pos);
+                draw(&field, &pos, *block);
             }
             Ok(Key::Char('q')) => {
                 show_cursor();
